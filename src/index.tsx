5864b6673b50c26bc7088e6b64d985a356c11249
João Moura <joao.moura@b-i.com>
2019-06-16 02:22:05 +0100

    Moved from babel and eslint to typescript and tslint

---
+++ src/index.tsx
@@ -0,0 +1,140 @@
+import React from 'react'
+
+// statics handler
+import hoistStatics from 'hoist-non-react-statics'
+
+let language: string = 'pt'
+let id: number = 1
+
+type Subscription = () => any
+
+const subscribes: {
+  [key: string]: Subscription
+} = {}
+
+interface ITranslation {
+  [key: string]: string | ITranslation
+}
+
+export interface ITranslations {
+  [key: string]: ITranslation
+}
+
+export type T = (path: string, args?: {[key: string]: string}) => string
+
+let translations: ITranslations = {}
+
+export function subscribe(cb: Subscription): number {
+  const newId = id
+  subscribes[newId] = cb
+  id += 1
+  return newId
+}
+
+export function unsubscribe(componentId: number): void {
+  delete subscribes[componentId]
+}
+
+function triggerSubscriptions() {
+  Object.keys(subscribes).forEach(
+    (componentId: string) => {
+      new Promise(
+        (resolve, reject) => {
+          subscribes[componentId]()
+          resolve()
+        }
+      ).then()
+    }
+  )
+}
+
+export function setDefaultLanguage(lang: string): void {
+  language = lang
+}
+
+export function setDefaultTranslations(userTranslations: ITranslations): void {
+  if (Object.keys(translations).length !== 0) {
+    setTranslations(userTranslations)
+    return
+  }
+  translations = userTranslations
+}
+
+export function setTranslations(userTranslations: ITranslations): void {
+  translations = userTranslations
+  triggerSubscriptions()
+}
+
+export function setLanguage(lang: string) {
+  language = lang
+  triggerSubscriptions()
+}
+
+export function getLanguage(): string {
+  return language
+}
+
+export function t(path: string, args?: {[key: string]: string}): string {
+  const translationKeys: string[] = path.split('.')
+  let translation: string = ''
+  let translationObj: ITranslation = translations[language]
+
+  translationKeys.forEach(
+    (key: string) => {
+      const temp: string | ITranslation = translationObj[key]
+      if (typeof translationObj[key] === 'object') {
+        translationObj = translationObj[key] as ITranslation
+      }
+      if (typeof temp === 'string') {
+        translation = temp
+      }
+    }
+  )
+
+  if (translation) {
+    if (args) {
+      Object.keys(args).forEach(
+        (key) => {
+          translation = translation.replace(`{${key}}`, args ? args[key] : '')
+        }
+      )
+    }
+  } else {
+    return path
+  }
+
+  return translation
+}
+
+export function translate(Component: React.ComponentType<any>): React.ComponentType<any> {
+  class TranslatedComponet extends React.Component<any, any> {
+    public id: number | undefined
+
+    public componentDidMount() {
+      this.id = subscribe(() => this.forceUpdate())
+    }
+
+    public componentWillUnmount() {
+      if (this.id) {
+        unsubscribe(this.id)
+      }
+    }
+
+    public render() {
+      return <Component {...this.props} t={t}/>
+    }
+  }
+
+  return hoistStatics(TranslatedComponet, Component)
+}
+
+export default {
+  setDefaultLanguage,
+  setLanguage,
+  setDefaultTranslations,
+  setTranslations,
+  translate,
+  subscribe,
+  unsubscribe,
+  t,
+}
